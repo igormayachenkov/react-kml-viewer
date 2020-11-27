cdf8d23ffd13f479c6e30f2a007fe5cfb7ac25e6
Igor Mayachenkov <igor.mayachenkov@gmail.com>
2020-11-27 20:05:45 +0300

    css moved to lib

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-exports.KmlViewer              = require("./lib/KmlViewer.js").KmlViewer;
-exports.parseFromDOMDocument   = require("./lib/KmlParser.js").parseFromDOMDocument;
+exports.KmlViewer              = require("./lib/KmlViewer.js").KmlViewer
+//exports.parseFromDOMDocument   = require("./lib/KmlParser.js").parseFromDOMDocument;
